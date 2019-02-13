d58f779951ea094d71f8d8b20387b6a8d03e2d9a
Brad Hughes <bradhugh@outlook.com>
2019-02-13 17:59:23 -0500

    Make a tiny back/close button

--- src/IotBbq.App/IotBbq.JsApp/src/app/components/home/home.component.ts
+++ src/IotBbq.App/IotBbq.JsApp/src/app/components/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ElectronService } from '../../providers/electron.service';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +8,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private electronService: ElectronService
+  ) { }
 
   ngOnInit() {
   }
 
+  public exitButtonClicked() {
+    if (this.electronService.isElectron()) {
+      this.electronService.remote.app.exit();
+    }
+  }
 }

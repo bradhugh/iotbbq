80b8d795779ad157f356ce9ac4d28f77ceb5eaeb
Brad Hughes <bradhugh@outlook.com>
2019-02-17 16:25:23 -0500

    Add public access modifier

--- src/IotBbq.App/IotBbq.JsApp/src/app/services/uwp/UwpSpiClient.ts
+++ src/IotBbq.App/IotBbq.JsApp/src/app/services/uwp/UwpSpiClient.ts
@@ -6,7 +6,7 @@ export class UwpSpiClient implements ISpiClient {
 
   private device: Windows.Devices.Spi.SpiDevice = null;
 
-  async initialize(chipSelectLine: number): Promise<void> {
+  public async initialize(chipSelectLine: number): Promise<void> {
     const settings = new Windows.Devices.Spi.SpiConnectionSettings(chipSelectLine);
     settings.clockFrequency = 1000000;
     settings.mode = Windows.Devices.Spi.SpiMode.mode0;
@@ -17,13 +17,13 @@ export class UwpSpiClient implements ISpiClient {
     this.device = await Windows.Devices.Spi.SpiDevice.fromIdAsync(deviceInfo[0].id, settings);
   }
 
-  async transfer(buffer: Uint8Array): Promise<Uint8Array> {
+  public async transfer(buffer: Uint8Array): Promise<Uint8Array> {
     const writeBuffer: number[] = Array.from(buffer);
     const readBuffer = this.device.transferFullDuplex(writeBuffer);
     return new Uint8Array(readBuffer);
   }
 
-  close(): void {
+  public close(): void {
     this.device.close();
   }
 }

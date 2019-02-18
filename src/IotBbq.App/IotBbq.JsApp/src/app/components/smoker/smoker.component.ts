08288cbce65669fb9de904bc58a8595aff61771a
Brad Hughes <bradhugh@outlook.com>
2019-02-18 10:14:22 -0500

    Make smoker monitor temperature

--- src/IotBbq.App/IotBbq.JsApp/src/app/components/smoker/smoker.component.ts
+++ src/IotBbq.App/IotBbq.JsApp/src/app/components/smoker/smoker.component.ts
@@ -1,6 +1,9 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, Inject } from '@angular/core';
 import { ISmokerModel } from '../../model/SmokerSettings';
 import { SmokerEditorService } from '../../services/SmokerEditorService';
+import { IThermometerService, THERM_SVC_TOKEN } from '../../services/IThermometerService';
+import { ALARM_SVC_TOKEN, IAlarmService, AlarmPriority } from '../../services/IAlarmService';
+import { Observable, timer } from 'rxjs';
 
 @Component({
   selector: 'app-smoker',
@@ -15,7 +18,21 @@ export class SmokerComponent implements OnInit {
     temperature: 0
   };
 
-  constructor(private smokerEditor: SmokerEditorService) { }
+  public isAlarming = false;
+
+  private alarmDelay = 1000; // * 60 * 5;
+
+  private loadedTime: Date;
+
+  private refreshTimer: Observable<number> = null;
+
+  private probeIndex = 7;
+
+  constructor(
+    private smokerEditor: SmokerEditorService,
+    @Inject(THERM_SVC_TOKEN) private thermometer: IThermometerService,
+    @Inject(ALARM_SVC_TOKEN) private alarmService: IAlarmService,
+    ) { }
 
   ngOnInit() {
     const settings = window.localStorage.getItem('smokerSettings');
@@ -23,9 +40,32 @@ export class SmokerComponent implements OnInit {
       this.model = JSON.parse(settings);
       this.model.temperature = 0;
     }
+
+    this.refreshTimer = timer(0, 1000);
+    this.refreshTimer.subscribe(async () => await this.onRefreshTimerTick());
+
+    this.loadedTime = new Date();
   }
 
   public async onSmokerComponentClicked() {
     await this.smokerEditor.editSettings(this.model);
+  }
+
+  private async onRefreshTimerTick(): Promise<void> {
+    const temps = await this.thermometer.readThermometer(this.probeIndex - 1);
+    this.model.temperature = temps.farenheight;
+
+    if (this.model.temperature >= this.model.highGate
+      || this.model.temperature < this.model.lowGate) {
+        const now = new Date();
+        if (now.getTime() - this.loadedTime.getTime() > this.alarmDelay) {
+          if (!this.isAlarming) {
+            this.isAlarming = true;
+            this.alarmService.triggerAlarm(AlarmPriority.Normal);
+          }
+        }
+      } else {
+        this.isAlarming = false;
+      }
   }
 }
